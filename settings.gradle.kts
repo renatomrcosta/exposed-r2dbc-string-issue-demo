2b12219ac49b34a17cced3718c514ffcbd70019e
Renato Costa <renato.rezende.costa@gmail.com>
2026-05-15 22:33:12 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+rootProject.name = "exposed-r2dbc-string-issue-demo"
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+package com.xunfos
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+}
